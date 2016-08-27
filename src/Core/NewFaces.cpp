2463dbb21bd75012dec3701e7dbf0a6ac656edca
Huynh Duc Dung <cs2014_dung.hd@student.vgu.edu.vn>
2016-08-27 16:02:16 +0700

    [Dung] Deal with new face

---
+++ src/Core/NewFaces.cpp
@@ -0,0 +1,18 @@
+/*
+ * NewFaces.cpp
+ *
+ *  Created on: Aug 27, 2016
+ *      Author: dung
+ */
+
+#include "NewFaces.h"
+
+NewFaces::NewFaces() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NewFaces::~NewFaces() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Core/NewFaces.h
@@ -0,0 +1,17 @@
+/*
+ * NewFaces.h
+ *
+ *  Created on: Aug 27, 2016
+ *      Author: dung
+ */
+
+#ifndef CORE_NEWFACES_H_
+#define CORE_NEWFACES_H_
+
+class NewFaces {
+public:
+	NewFaces();
+	virtual ~NewFaces();
+};
+
+#endif /* CORE_NEWFACES_H_ */
